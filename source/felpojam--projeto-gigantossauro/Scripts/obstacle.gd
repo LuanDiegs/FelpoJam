d759777e379280b3e86ad0fdca065334e54ee6aa
Mordchan <mordecai.girassol@gmail.com>
2026-02-17 03:04:43 -0300

    .

--- source/felpojam--projeto-gigantossauro/Scripts/obstacle.gd
+++ source/felpojam--projeto-gigantossauro/Scripts/obstacle.gd
@@ -1,8 +1,8 @@
 extends StaticBody2D
 
-
+#Função de colisão da area 2D dele (Vai ser usada para fazer coisas com o player quando ele colidir)
 func _on_area_body_entered(body: Node2D) -> void:
 	
+	   #Checa se o corpo que colidiu está no grupo "Player
 	if body.is_in_group("Player"):
-		
-		pass
+		pass #faz nada ainda
